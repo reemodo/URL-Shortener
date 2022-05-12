3b11429d781300fa5296d01418a957c3ec1d5816
Reem Hikmat <49403027+reemodo@users.noreply.github.com>
2022-05-12 14:16:29 +0300

    install mongodb

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,14 @@
-from flask import Flask, request ,render_template
-app = Flask(__name__)
 
-@app.route('/',methods=['POST','GET'])
+from flask import Flask, request ,render_template
+from pymongo import MongoClient
+
+cluster = "mongodb+srv://Reem:1234pipi@cluster0.jaez9.mongodb.net/ShortenerUrl_DB?retryWrites=true&w=majority"
+client = MongoClient(cluster)
+
+print(client.list_database_names())
+db= client.test
+app = Flask(__name__)
+@app.python 
 def hello():
     if request.method == "POST" :
         urlname = request.form["nm"]
